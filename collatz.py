cccd46d4a96951abf0996b14c7e1d2045cfd5b86
Physics Data Labs <physics-data@cslab.moe>
2024-07-03 16:36:21 +0800

    Import from repo collatz@9844085

---
+++ collatz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+n = int(input())
+
+# TODO: run the loop
+
+# TODO: print total number of step
+
+# TODO: print the max number encountered in the loop
