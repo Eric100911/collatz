eb30b0777e5f160dc77cb2f4ed3f8a6a17672e05
Eric100911 <eric100911@126.com>
2024-07-03 17:04:29 +0800

    :sparkles: Completed the python script for problem "collatz"
    :while_check_mark: Passed check from grade.py
    :loud_sound: Replaced the division "/" with "//" to make it work. (Rather puzzling!) Suspecting that the limited precision of float type caused the fault when using "/".

--- collatz.py
+++ collatz.py
@@ -2,9 +2,21 @@
 # -*- coding: utf-8 -*-
 
 n = int(input())
+loopCount = 0
+maxn = n
 
 # TODO: run the loop
 
+while n != 1 :
+    loopCount += 1
+    if n % 2 == 0 :
+        n = n // 2
+    else:
+        n = n * 3 + 1
+        maxn = max(n, maxn)
+
 # TODO: print total number of step
+print(loopCount)
 
 # TODO: print the max number encountered in the loop
+print(maxn)
